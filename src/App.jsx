f6a6eecc571c90a73c519d52706cdd181c38ef31
subhanabdi <subhanabidi211@gmail.com>
2023-12-22 00:21:09 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { PAGES } from "./utils/pages";
+import Generator from "./pages/Generator";
+import Profile from "./pages/Profile";
+import { loadData } from "./utils/localStorage";
 
-export default function App() {
-  return (
-    <div>App</div>
-  )
+function App() {
+  // State management
+  const [page, setPage] = useState(PAGES.GENERATOR);
+  const [openAIKey, setOpenAIKey] = useState();
+  const [resume, setResume] = useState();
+
+  // Load data from local storage on component mount
+  useEffect(() => {
+    const fetchLocalData = async () => {
+      const localResume = await loadData("resume");
+      const localOpenAIKey = await loadData("openAIKey");
+
+      setResume(localResume);
+      setOpenAIKey(localOpenAIKey);
+    };
+
+    fetchLocalData();
+  }, []);
+
+  // Render components based on the current page
+  switch (page) {
+    case PAGES.GENERATOR:
+      return (
+        <Generator setPage={setPage} resume={resume} openAIKey={openAIKey} />
+      );
+
+    case PAGES.PROFILE:
+      return (
+        <Profile
+          setPage={setPage}
+          setOpenAIKey={setOpenAIKey}
+          setResume={setResume}
+          resume={resume}
+          openAIKey={openAIKey}
+        />
+      );
+
+    default:
+      return (
+        <Generator setPage={setPage} resume={resume} openAIKey={openAIKey} />
+      );
+  }
 }
+
+export default App;
